dd8b52b07cf8a75688c939c1453344f2b85431af
Ratul <ratul.bside@gmail.com>
2021-02-08 17:00:55 +0600

    New font and updates by Zia

--- app/public/wp-content/themes/wp-bootstrap-starter-child/page-sushil.php
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-sushil.php
@@ -4,55 +4,65 @@
 get_header();
 ?>
 
-    <section id="primary" class="content-area">
-        <div id="main" class="site-main" role="main">
-            <div class="container">
-                <h2 style="width: 100%; text-align: center;">Sushil Jagtap</h2>
+    
+            <div class="container-sushil">
+                <!-- <h2 style="width: 100%; text-align: center;">Sushil Jagtap</h2> -->
                 <div class="single-background sushil">
                     <div class="single-row one">
-                        <div class="single-row-left"></div>
-                        <div class="single-row-right popup-gallery">
+                        <div class="single-row-left">
+                            <h3 class="single-name">NAME: SUSHIL JAGTAP</h3>
+                        </div>
+                        <div class="single-row-right">
                             
                         </div>
                     </div>
                     <div class="single-row two">
-                        <div class="single-row-left popup-gallery">
+                        <div class="single-row-left">
                             
                         </div>
                         <div class="single-row-right">
-                            
+                            <p class="single-concept-note">
+                                Concept Note- To stay safe from Corona we adopted the Lockdown as a self-responsibility. Being in closed spaces became intolerable and we eagerly waited for it to end. Through this video, I have tried to show the impatience each of us had for the Lockdown to get over.
+                            </p>
                         </div>
                     </div>
                     <div class="single-row three">
                         <div class="single-row-left">
-                            <a class="popup-video" href="https://www.youtube.com/watch?v=QT_JklxIg6Y">
-                                <img id="varsha-two" src="<?php echo get_theme_file_uri(); ?>/images/Sushil.jpg">
-                            </a>
-                        </div>
-                        <div class="single-row-right popup-gallery">
-                            
-                        </div>
-                    </div>
-                    <div class="single-row four">
-                        <div class="single-row-left popup-gallery">
+                            <h3 class="single-institute">College: DR.B.A.M.U.A</h3>
                             
                         </div>
                         <div class="single-row-right">
+                            <h3 class="single-medium">MEDIUM - videos</h3>
+                        </div>
+                    </div>
+                    <div class="single-row four">
+                        <div class="single-row-left">
+                            <a class="popup-video" href="https://www.youtube.com/watch?v=QT_JklxIg6Y">
+                                <img id="varsha-two" src="<?php echo get_theme_file_uri(); ?>/images/Sushil.jpg">
+                            </a>
+                            <h3 class="single-title">Title: sound</h3>
+                        </div>
+                        <div class="single-row-right">
+                            <h3 class="single-location">City: pune</h3>
+                        </div>
+                    </div>
+                    <div class="single-row five">
+                        <div class="single-row-left">
                             
+                        </div>
+                        <div class="single-row-right">
+                            <h3 class="single-duration">Duration: 1.48</h3>
                         </div>
                     </div>
                 </div>
-                <div class="single-concept-note">
-                    <p>In the above painting, I wanted to mentioned that what is the condition of people in lockdown and how people are lived their life dramatically , drastically,  and may be decisively . The lockdown applied to three main areas physical movement out of the home,  social distancing when people outside home and restricted availability of most public services. All people having same rule "stay home and stay safe." Fish are packed in the pot. she can not surview anywhere same way the people having same condition like fish. Here I only said that it is safe when you are in home and it might be risky if u go out of home.</p>
-                </div>
+                <?php 
+                    get_template_part( 'template-parts/single-switch', 'switch' );
+
+                ?>
             </div>
 
-            <?php 
-                get_template_part( 'template-parts/single-switch', 'switch' );
-
-             ?>
-        </div><!-- #main -->
-    </section><!-- #primary -->
+            
+        
 
     <script type="text/javascript">
        jQuery(function() {
