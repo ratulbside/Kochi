7f11878de08d94d5b9fc8b913081554868392e64
Ratul <ratul.bside@outlook.com>
2021-02-11 03:36:48 +0600

    Chaitanya - mod needed

--- app/public/wp-content/themes/wp-bootstrap-starter-child/page-chaitanya.php
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-chaitanya.php
@@ -4,16 +4,14 @@
 get_header();
 ?>
 
-    <section id="primary" class="content-area">
-        <div id="main" class="site-main" role="main">
-            <div class="container">
-                <h2 style="width: 100%; text-align: center;">Chaitanya Powle</h2>
+    
+            <div class="container-chaitanya">
+                <!-- <h2 style="width: 100%; text-align: center;">Chaitanya Powle</h2> -->
                 <div class="single-background chaitanya">
                     <div class="single-row one">
                         <div class="single-row-left popup-gallery">
-                            <a href="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/1.jpeg">
-                                <img src="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/1.jpeg">
-                            </a>
+							<h3 class="single-name">Chaitanya Powle</h3>
+							<h3 class="single-institute">Vasai Vikasini College of Visual Art</h3>
                         </div>
                         <div class="single-row-right">
                             <a class="popup-video" href="https://www.youtube.com/watch?v=kYrJfYbePFE">
@@ -31,37 +29,33 @@ get_header();
                     </div>
                     <div class="single-row three">
                         <div class="single-row-left"></div>
-                        <div class="single-row-right popup-gallery">
-                            <a href="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/2.jpeg">
-                                <img src="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/2.jpeg">
-                            </a>
+                        <div class="single-row-right">
+							<h3 class="single-location">Mumbai</h3>
+							<h3 class="single-duration">Duration-</h3>
                         </div>
                     </div>
                     <div class="single-row four">
                         <div class="single-row-left popup-gallery">
+                            <h3 class="single-title">grown fungus</h3>
                              <a href="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/3.jpeg">
                                 <img src="<?php echo get_theme_file_uri(); ?>/images/Chaitanya Powle -rez/3.jpeg">
                             </a>
                         </div>
                         <div class="single-row-right">
-                            
+							<div class="single-concept-note">
+								<p>Concept- whole world is been locked due to the pandemic of covid-19. Because of this the whole world is locked in their own houses.due to this, footwear which we wear are useless. Eventually, footwear is a thing which we wear in our daily life.</p>
+
+		<p>Humans got the habit of getting slept in their houses,this habit made humans so lazy that their body contained much amount of fats in their body. This pandemic made humans used too it in their daily routine.due to this, the footwear started getting fungus in it. Months and months made the fungus large in size.</p>
+							</div>
                         </div>
                     </div>
                 </div>
-                <div class="single-concept-note">
-                    <p>Concept- whole world is been locked due to the pandemic of covid-19. Because of this the whole world is locked in their own houses.due to this, footwear which we wear are useless. Eventually, footwear is a thing which we wear in our daily life.</p>
-
-                    <p>Humans got the habit of getting slept in their houses,this habit made humans so lazy that their body contained much amount of fats in their body. This pandemic made humans used too it in their daily routine.due to this, the footwear started getting fungus in it. Months and months made the fungus large in size.</p>
-
-                </div>
-            </div>
-
-            <?php 
+                <?php 
                 get_template_part( 'template-parts/single-switch', 'switch' );
 
              ?>
-        </div><!-- #main -->
-    </section><!-- #primary -->
+            
+			</div>
 
     <script type="text/javascript">
        jQuery(function() {
