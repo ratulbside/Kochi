fc867733013ca50ac7f28943d6806d4962598cb9
raselAhamed007 <raseluf5@gmail.com>
2021-02-11 20:22:12 +0600

    afternoon work

--- app/public/wp-content/themes/wp-bootstrap-starter-child/page-rohit.php
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-rohit.php
@@ -5,11 +5,11 @@ get_header();
 ?>
 
     <div class="container-rohit">
-                <h2 style="width: 100%; text-align: center;">Rohit</h2>
+                
                 <div class="single-background rohit">
                     <div class="single-row one">
                         <div class="single-row-left">
-                            
+                            <h3 class="single-name">Rohit Varekar</h3>
                         </div>
                         <div class="single-row-right">
                             <a class="popup-video" href="https://www.youtube.com/watch?v=kqoVZFX-rEA">
@@ -38,16 +38,15 @@ get_header();
                         </div>
                     </div>
                     <div class="single-row four">
-                        <div class="single-row-left popup-gallery">
-                            
+                        <div class="single-row-left">
+                            <p class="single-concept-note">
+                                To stay safe from Corona we adopted the Lockdown as a self-responsibility. Being in closed spaces became intolerable and we eagerly waited for it to end. Through this video, I have tried to show the impatience each of us had for the Lockdown to get over.
+                            </p>
                         </div>
                         <div class="single-row-right">
                             
                         </div>
                     </div>
-                </div>
-                <div class="single-concept-note">
-                    <p>In the above painting, I wanted to mentioned that what is the condition of people in lockdown and how people are lived their life dramatically , drastically,  and may be decisively . The lockdown applied to three main areas physical movement out of the home,  social distancing when people outside home and restricted availability of most public services. All people having same rule "stay home and stay safe." Fish are packed in the pot. she can not surview anywhere same way the people having same condition like fish. Here I only said that it is safe when you are in home and it might be risky if u go out of home.</p>
                 </div>
                 <?php 
                 get_template_part( 'template-parts/single-switch', 'switch' );
