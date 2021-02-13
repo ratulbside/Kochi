510147e5fd40a70a194fa973b81ff7d68b5c8a4c
Ratul <ratul.bside@outlook.com>
2021-02-14 00:46:37 +0600

    Kashin page renaming and mentor page

---
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-kashin.php
@@ -0,0 +1,82 @@
+<?php
+
+
+get_header();
+?>
+<style type="text/css">
+body {
+    background-color: #b19b83;
+}
+</style>
+<div class="container-kasin">
+    <div class="single-background kasin">
+        <div class="single-row one">
+            <div class="single-row-left">
+                <h3 class="single-name">Kashin Patel</h3>
+                <h3 class="single-institute">Sir J. J. School of Art</h3>
+                <h3 class="single-medium">videos</h3>
+            </div>
+            <div class="single-row-right">
+                <a class="popup-video" href="https://www.youtube.com/watch?v=dQWg9QoWt7c">
+                    <img id="varsha-two" src="<?php echo get_theme_file_uri(); ?>/images/Kashin.jpg">
+                </a>
+                <h3 class="single-title">sound</h3>
+            </div>
+        </div>
+        <div class="single-row two">
+            <div class="single-row-left">
+                <div class="single-concept-note">
+                    <p>In the above painting, I wanted to mentioned that what is the condition of people in lockdown and
+                        how people are lived their life dramatically , drastically, and may be decisively . The lockdown
+                        applied to three main areas physical movement out of the home, social distancing when people
+                        outside home and restricted availability of most public services. All people having same rule
+                        "stay home and stay safe." Fish are packed in the pot. she can not surview anywhere same way the
+                        people having same condition like fish. Here I only said that it is safe when you are in home
+                        and it might be risky if u go out of home.</p>
+                </div>
+            </div>
+            <div class="single-row-right">
+
+            </div>
+        </div>
+        <div class="single-row three">
+            <div class="single-row-left">
+                <h3 class="single-location">Pune</h3>
+            </div>
+            <div class="single-row-right">
+                <h3 class="single-duration">Duration: 1.48</h3>
+            </div>
+        </div>
+        <div class="single-row four">
+            <div class="single-row-left">
+
+            </div>
+            <div class="single-row-right">
+
+            </div>
+        </div>
+    </div>
+    <?php 
+                get_template_part( 'template-parts/single-switch', 'switch' );
+
+             ?>
+
+</div>
+
+<?php
+get_footer();
+?>
+
+<script type="text/javascript">
+jQuery(function() {
+    jQuery('.popup-video').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
+</script>
--- app/public/wp-content/themes/wp-bootstrap-starter-child/page-mentor.php
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-mentor.php
@@ -17,10 +17,30 @@
                     <div class="container">
                         <header class="entry-header">
                             <h1 class="entry-title">
-                                A wide gap (of) remains
+                            Workshop Facilitator Note </br>
+                            Amol K Patil 
                             </h1>
                         </header>
                         <div class="entry-content mentor-videos">
+                            <div class="row">
+                                <div class="col-12">
+                                    <p>
+                                    This workshop, which is a part of the 5th edition of the Students’ Biennale, was a collective effort to explore digital platforms and a reaction to periods of isolation. We used interactive exercises to explore these possibilities in virtual spaces. 
+                                    </p>
+                                    <p>
+                                    In the period of the lockdown, and through the pandemic, all of us made some similar observations. During these exercises, we attempted to connect these. Particularly, the smaller moments that get lost easily or get overlapped with those of a higher amplitude. For example, when we think about it, the immediate sound of birds chirping may reach our ears. Nowadays, we probably wake up listening to them. They sabotaged the sound of horns and every single machine that was out there claiming their own space in the environment. 
+                                    </p>
+                                    <p>
+                                    Even in the personal space, it is quite interesting to see how through yoga, exercise and meditation, people are giving themselves hope. This is also because of how we define humans as social animals and when not being able to meet each other, we could not overlook the wider social aspect that was happening outside of our homes. Significantly with the labors- how the body is transporting itself from one city to another without food and surety to reach its destination which makes us question this structure and the position of the migrants itself.
+                                    </p>
+                                    <p>
+                                    All the participants and I, together, tried to encounter these challenges. We addressed the sense of touch which was very prominently replaced by the sense of seeing. These days the most exploitative organ in our body is our eyes. We try to touch through our eyes, not by actually touching anything, but just by looking. We have a very intimate relationship with gadgets. Especially cameras. We perform in front of the camera. In our sessions, we went through a series of live performances on camera to interact with each other and exchange expressions. 
+                                    </p>
+                                    <p>
+                                    Eventually, we tried collaborative as well as individual exercises to explore different mediums. For example- painting, digital drawings, video, stop motion, animation, etc. Thinking of the white cube legacy, we offer an alternative space to present our work in a more intimate space. We are inviting viewers to experience the exquisite space of each participant's home on the website. This is a collective expression by a group of emerging artists from India.
+                                    </p>
+                                </div>
+                            </div>
                             <div class="row">
                                 <div class="col-12 col-md-4">
                                     <a class="popup-video" href="https://www.youtube.com/watch?v=SOHqbE6W2aI">
