d6d2b9c5051fcd75b713d466d5ba66e0286f1c09
raselAhamed007 <raseluf5@gmail.com>
2021-02-15 01:47:08 +0600

    update tanya details on mobile

--- app/public/wp-content/themes/wp-bootstrap-starter-child/page-tanya.php
+++ app/public/wp-content/themes/wp-bootstrap-starter-child/page-tanya.php
@@ -9,6 +9,16 @@ body {
 </style>
             <div class="container-tanya">
                 <div class="single-background tanya">
+                    <span id="view-tanya" class="view-res-btn">View Details</span>
+                    <div id="content-popup" class="white-popup mfp-hide">
+                      <h4>Name: Tanya Badkul</h4>
+                      <h4>Institute: Govt.college of art and design ,Nagpur</h4>
+                      <h4>Location: Jabalpur</h4>
+                      <h4>Title: UNCERTAINTY</h4>
+                      <h4>Medium: Videos</h4>
+                      <h4>Duration: 2.42</h4>
+                      <p>Concept Note: <br>The lockdown has forced all of us to slow down, and has even created anxiety and restlessness in some of us.<br>I would roam around the house, aimlessly, as my body and mind were used to space and movement. It took me time to develop new habits in this “new normal”. <br>I began meditating on life and it’s uncertainty, which I have attempted to depict in my work. Just as when one creates a house of cards- cautiously and carefully- with a lingering uncertainty on whether or not it will fall, this uncertainty applies to our lives as well. </p>
+                    </div>
                     <div class="single-row one">
                         <div class="single-row-left">
                              <h3 class="single-name">Tanya Badkul</h3>
@@ -70,6 +80,16 @@ get_footer();
                 preloader: false,
 
                 fixedContentPos: false
+            });
+
+            jQuery('#view-tanya').on("click", function(){
+                jQuery.magnificPopup.open({
+                  items: {
+                      src: '#content-popup',
+                      type:'inline'
+                  },
+                  modal: false
+              });
             });
         });
     </script>
